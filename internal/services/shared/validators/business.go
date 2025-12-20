a30ab76501acd7067941a006c3c4030512bc4b06
Eduardo Henrique de Paula Moro <46352760+eduardo-moro@users.noreply.github.com>
2025-12-20 17:19:12 -0300

    tests: Created validation tests

--- internal/services/shared/validators/business.go
+++ internal/services/shared/validators/business.go
@@ -2,45 +2,78 @@ package validators
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"os"
+	"time"
 	"trading/internal/domain"
 )
 
 // BusinessValidator implementa validações de regras de negócio
 type BusinessValidator struct {
 	// TODO: Implementar campos necessários (ex: stocks map)
-	stocks map[string]Stock
+	Metadata     Metadata         `json:"metadata"`
+	Stocks       map[string]Stock `json:"stocks"`
+	MarketHours  MarketHours      `json:"market_hours"`
+	TradingRules TradingRules     `json:"trading_rules"`
+}
+
+type Metadata struct {
+	Version      string `json:"version"`
+	LastUpdated  string `json:"last_updated"`
+	Market       string `json:"market"`
+	Currency     string `json:"currency"`
+	TotalSymbols int    `json:"total_symbols"`
+	Description  string `json:"description"`
 }
 
 type Stock struct {
-	company     string  `json:"company"`
-	sector      string  `json:"sector"`
-	minPrice    float64 `json:"min_price"`
-	marketCap   string  `json:"market_cap"`
-	description string  `json:"description"`
+	Company     string  `json:"company"`
+	Sector      string  `json:"sector"`
+	MinPrice    float64 `json:"min_price"`
+	MarketCap   string  `json:"market_cap"`
+	Description string  `json:"description"`
+}
+
+type MarketHours struct {
+	Timezone     string       `json:"timezone"`
+	RegularHours regularHours `json:"regular_hours"`
+	ClosedDays   []string     `json:"closed_days"`
+	Holidays     []string     `json:"holidays"`
+}
+
+type regularHours struct {
+	Open  string   `json:"open"`
+	Close string   `json:"close"`
+	Days  []string `json:"days"`
+}
+
+type TradingRules struct {
+	MinOrderValue float64 `json:"min_order_value"`
+	TickSize      float64 `json:"tick_size"`
+	LotSize       float64 `json:"lot_size"`
 }
 
 // NewBusinessValidator cria um novo validador de negócio
 func NewBusinessValidator() *BusinessValidator {
 	validator := &BusinessValidator{
 		// TODO: Inicializar campos
-		stocks: make(map[string]Stock),
+		Metadata:     Metadata{},
+		Stocks:       make(map[string]Stock),
+		MarketHours:  MarketHours{},
+		TradingRules: TradingRules{},
 	}
 
 	// TODO: Carregar dados de ações do arquivo JSON
-	content, err := os.ReadFile("../../../data/stocks.json")
+	content, err := os.ReadFile("../../../../data/stocks.json")
 	if err != nil {
 		log.Fatal("Error when opening stocks file: ", err)
 	}
-	err = json.Unmarshal(content, &validator.stocks)
+	err = json.Unmarshal(content, &validator)
 	if err != nil {
 		log.Fatal("Error when parsing stocks file: ", err)
 	}
 
-	for _, stock := range validator.stocks {
-		log.Println(stock.description)
-	}
 	return validator
 }
 
@@ -49,12 +82,12 @@ func (v *BusinessValidator) ValidateOrder(order *domain.Order) error {
 	// TODO: Implementar validações completas
 	if order == nil {
 		log.Println("Order is nil")
-		return nil
+		return errors.New("order cannot be nil")
 	}
 
 	var orderStock Stock
 
-	for symbol, stock := range v.stocks {
+	for symbol, stock := range v.Stocks {
 		if symbol == order.Symbol {
 			orderStock = stock
 		}
@@ -63,18 +96,56 @@ func (v *BusinessValidator) ValidateOrder(order *domain.Order) error {
 	// 1. Validar símbolo existe
 	if (Stock{}) == orderStock {
 		log.Printf("Stock %s not found", order.Symbol)
-		return nil
+		return errors.New("order stock not found")
 	}
 
 	// 2. Validar preço mínimo
-	if order.Price <= orderStock.minPrice {
+	if order.Price < orderStock.MinPrice {
 		log.Printf("Order %s is lower than the minimum price", order.Symbol)
-		return nil
+		return errors.New("order is lower than the minimum price")
 	}
 
-	
+	if order.Price < v.TradingRules.MinOrderValue {
+		log.Printf("Order %s is lower than the minimum price", order.Symbol)
+		return errors.New("order is lower than the minimum price")
+	}
+
 	// 3. Validar horário de mercado
-	// 4. Validar campos básicos
+	now := time.Now()
+	openTime, err := time.Parse("15:04", v.MarketHours.RegularHours.Open)
+	if err != nil {
+		log.Printf("Error when parsing open time: %v", err)
+		return errors.New("could not parse open time")
+	}
+	closeTime, err := time.Parse("15:04", v.MarketHours.RegularHours.Close)
+	if err != nil {
+		log.Printf("Error when parsing close time: %v", err)
+		return errors.New("could not parse close time")
+	}
+
+	if now.Before(openTime) || now.After(closeTime) {
+		log.Printf("Invalid operation time: %v", order.Symbol)
+		return errors.New("market is closed")
+	}
+
+	found := false
+	for day := range v.MarketHours.RegularHours.Days {
+		if day == now.Day() {
+			found = true
+		}
+	}
+
+	if !found {
+		log.Printf("Invalid operation day: %v", order.Symbol)
+		return errors.New("market is closed")
+	}
+
+	for day := range v.MarketHours.Holidays {
+		if day == now.Day() {
+			log.Printf("Invalid operation day, it's a holiday!: %v", order.Symbol)
+			return errors.New("market is closed")
+		}
+	}
 
 	return nil
 }
---
+++ internal/services/shared/validators/business_test.go
@@ -0,0 +1,48 @@
+package validators
+
+import (
+	"testing"
+	"time"
+	"trading/internal/domain"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewBusinessValidator(t *testing.T) {
+	businessValidator := NewBusinessValidator()
+
+	assert.NotNil(t, businessValidator)
+}
+
+func TestBusinessValidator_ValidateOrder(t *testing.T) {
+	var testCases = []struct {
+		Symbol   string
+		Price    float64
+		FakeTime string
+		Want     bool
+	}{
+		{"AAPL", 180, "2025-08-10 12:00", false},
+		{"ABC", 180, "2025-08-10 12:00", false},
+		{"AAPL", 200, "2025-08-10 12:00", true},
+		{"AAPL", 2_800_000_000_000_000, "2025-08-10 12:00", true},
+	}
+	businessValidator := NewBusinessValidator()
+
+	assert.NotNil(t, businessValidator)
+	order := &domain.Order{
+		ID:        "123",
+		UserID:    "123",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Quantity:  1,
+		Status:    domain.PENDING,
+	}
+
+	for _, testCase := range testCases {
+		order.Symbol = testCase.Symbol
+		order.Price = testCase.Price
+
+		result := businessValidator.ValidateOrder(order) == nil
+		assert.Equal(t, testCase.Want, result)
+	}
+}
